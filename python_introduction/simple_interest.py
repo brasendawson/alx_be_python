78dd313246941c105ec52d55695ff92b239877c3
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-09-11 12:42:11 +0000

    alx

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-P = 1000
-R = 0.05
-T = 3
+principal = 1000
+rate = 0.05
+time = 3
 
-I = P * R * T
-print(f"The Simple interest is: {I}")
+interest = principal * rate * time
+print("The Simple interest is: ",interest)
