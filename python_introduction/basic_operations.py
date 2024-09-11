0970fe12183d5d40e17a772e03fdd3da82170e71
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-09-11 12:33:43 +0000

    alx

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,10 +2,10 @@ number1 = 10
 number2 = 5
 
 sum_result = number1 + number2
-print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"Addition of {number1} and {number2} is {sum_result}")
 
 difference_result = number1 - number2
-print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Subtraction of {number1} and {number2} is {difference_result}")
 
 product_result = number1 * number2
-print(f"Product of {number1} and {number2} is {product_result}")
+print(f"Multiplication of {number1} and {number2} is {product_result}")
