5d82b1b8522a94820e7783912dbebbbfd42941af
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-09-11 13:19:38 +0000

    alx

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthy_income = int(input("Enter your monthly income: "))
 monthy_expenses = int(input("Enter your total monthly expenses: "))
 
-monthy_savings = monthy_income - monthy_expenses
+monthy_savings = (monthy_income - monthy_expenses)
 print("Your Monthly savings are $",monthy_savings,".")
 
 projected_savings = (monthy_savings * 12 + (monthy_savings) * 12 * 0.05)
