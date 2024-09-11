5dbcdd5da59b82f79ac02d128034ad6572aec4e0
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-09-11 16:44:33 +0000

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -13,7 +13,7 @@ monthly_savings = monthly_income - monthly_expenses
 annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Display the user's monthly savings
-print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Your monthly savings are ${monthly_savings}.")
 
 # Display the projected annual savings with interest
-print(f"Projected savings after one year, with interest, is: ${annual_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
