cf35553ad5c99c8abb6986b6a6713eda4bb3a6db
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-09-18 09:37:15 +0000

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,5 @@
-num1 = int(input("Enter the first number: \n"))
-num2 = int(input("Enter the second number: \n"))
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
 operation = input("Choose the operation (+, -, *, /): \n")
 
 
