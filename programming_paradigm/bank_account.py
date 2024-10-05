6dd824e6c8bd7cf8f8455c982bb82e261d837e27
Brasen Ethan Kwame Dawson <139929813+brasendawson@users.noreply.github.com>
2024-10-05 08:33:41 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,20 +1,16 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        # Initialize the account balance, default to zero if not provided
         self.__account_balance = initial_balance
 
     def deposit(self, amount):
-        # Adds the deposit amount to the account balance
         if amount > 0:
             self.__account_balance += amount
 
     def withdraw(self, amount):
-        # Deducts the amount from the account balance if sufficient funds exist
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
         return False
 
     def display_balance(self):
-        # Displays the current account balance in a user-friendly format
         print(f"Current Balance: ${self.__account_balance:.2f}")
